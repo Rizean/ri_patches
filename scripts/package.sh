c2d991820e612a7c6f58a566ad85b3b92fe5bfca
root <root@michael-workpc.localdomain>
2021-01-15 20:22:57 -0500

    prefix ri_patch to folders to avoid merging the files with original mod

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,12 +2,12 @@
 mkdir -p dist
 cd build
 pwd
-zip -9 -r ri_patch_sb_BetterPregnancy.zip sb_BetterPregnancy/*.*
-zip -9 -r ri_patch_sb_PartyAndNightlife.zip sb_PartyAndNightlife/*.*
-zip -9 -r ri_patch_sb_PimpYourGirl.zip sb_PimpYourGirl/*.*
-zip -9 -r ri_patch_sb_TheNewGoodbye.zip sb_TheNewGoodbye/*.*
-zip -9 -r ri_patch_vin_Base.zip vin_Base/*.*
-zip -9 -r ri_patch_vin_Incest.zip vin_Incest/*.*
-zip -9 -r ri_patch_vin_Pregnancy.zip vin_Pregnancy/*.*
+zip -9 -r ri_patch_sb_BetterPregnancy.zip       ri_patch_sb_BetterPregnancy/*.*
+zip -9 -r ri_patch_sb_PartyAndNightlife.zip     ri_patch_sb_PartyAndNightlife/*.*
+zip -9 -r ri_patch_sb_PimpYourGirl.zip          ri_patch_sb_PimpYourGirl/*.*
+zip -9 -r ri_patch_sb_TheNewGoodbye.zip         ri_patch_sb_TheNewGoodbye/*.*
+zip -9 -r ri_patch_vin_Base.zip                 ri_patch_vin_Base/*.*
+zip -9 -r ri_patch_vin_Incest.zip               ri_patch_vin_Incest/*.*
+zip -9 -r ri_patch_vin_Pregnancy.zip            ri_patch_vin_Pregnancy/*.*
 cp ./*.zip ../dist
 #sleep 30
